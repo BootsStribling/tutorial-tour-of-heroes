107be7825998462a29ff9834f61c0cf960c6bd28
BootsStribling <williamstriblingenterprises@gmail.com>
2022-11-22 15:05:32 -0600

    add mock heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero.js'
+import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   
-  hero: Hero = {
-  
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
 
   constructor() { }
 
